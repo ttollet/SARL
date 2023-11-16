c05249975e8851bdb3f5e195ae835e45f557edd7
Theodore Tollet <ttollet@icloud.com>
2023-11-16 17:29:53 +0000

    Preliminary test success

--- tests/test_pdqn.py
+++ tests/test_pdqn.py
@@ -10,90 +10,183 @@ from sarl.common.bester.common.goal_domain import GoalObservationWrapper
 from sarl.common.bester.common.goal_domain import GoalFlattenedActionWrapper
 
 from sarl.common.bester.environments.gym_platform.envs import PlatformEnv
-# from sarl.common.bester.common.platform_domain import PlatformFlattenedActionWrapper
+from sarl.common.bester.common.platform_domain import PlatformFlattenedActionWrapper
 
 
-def test_pdqn_goal(train_episodes=1, max_steps=201, num_seeds=3):
-    '''Ensure Q-PAMDP learns within Goal'''
-    for seed in range(num_seeds):
-        np.random.seed(seed)
+def _make_env(env_name: str, max_steps: int, seed: int):
+    env = gym.make(env_name)
+    env = RecordEpisodeStatistics(env, deque_size=max_steps)  # Note stats
+    env.seed(seed)  # Remove stochasticity
+    np.random.seed(seed)
+    return env
 
+
+def _pad_action(act, act_param):
+    params = [np.zeros((2,)), np.zeros((1,)), np.zeros((1,))]
+    params[act] = act_param
+    return (act, params)
+
+
+def _get_training_info(train_episodes, agent, env, max_steps, seed, pad_action, reward_scale=1):
+    '''Train to output a list of returns by timestep.'''
+    info_per_episode = []
+    for _ in range(train_episodes):
+        (observation, steps), info = env.reset(seed=seed)
+        observation = np.array(observation, dtype=np.float32, copy=False)
+        act, act_param, all_action_parameters = agent.act(observation)
+        action = pad_action(act, act_param)
+
+        # Episode loop
+        agent.start_episode()
+        for j in range(max_steps):
+            (next_observation, steps), reward, terminated, truncated, info = env.step(action)
+            next_observation = np.array(next_observation, dtype=np.float32, copy=False)
+
+            next_act, next_act_param, next_all_action_parameters = agent.act(next_observation)
+            next_action = pad_action(next_act, next_act_param)
+
+            r = reward * reward_scale
+            agent.step(observation, (act, all_action_parameters), r, next_observation, (next_act, next_all_action_parameters), terminated or truncated, steps)
+
+            # Reassign time-dependant variables
+            act, act_param, all_action_parameters = next_act, next_act_param, next_all_action_parameters
+            action = next_action
+            observation = next_observation
+
+            if terminated or truncated:
+                break
+        agent.end_episode()
+        info_per_episode.append(info)
+    env.close()
+    return info_per_episode
+
+
+def test_pdqn_platform(train_episodes=2500, max_steps=250, seeds=[1]):
+    '''Ensure P-DQN learns within Platform'''
+    for seed in seeds:
         # Environment initialisation
-        env = gym.make("Goal-v0")
-        env = RecordEpisodeStatistics(env, deque_size=max_steps)  # Note stats
-        env.seed(seed)  # Remove stochasticity
+        env = _make_env(env_name="Platform-v0", max_steps=max_steps, seed=seed)
 
-        # Feature engineering | TODO: Convert to environment options
-        env = GoalObservationWrapper(env)
-        env = GoalFlattenedActionWrapper(env)
-        env = ScaledStateWrapper(env)  # Observations -> [-1,1]
+        # Setup
+        ## Values
+        initial_params_ = [3., 10., 400.]
+        ## Scaled actions
+        for a in range(env.action_space.spaces[0].n):
+            initial_params_[a] = 2. * (initial_params_[a] - env.action_space.spaces[1].spaces[a].low) / (
+                        env.action_space.spaces[1].spaces[a].high - env.action_space.spaces[1].spaces[a].low) - 1.
+        ## Initial weights
+        initial_weights = np.zeros((env.action_space.spaces[0].n, env.observation_space.spaces[0].shape[0]))
+        initial_bias = np.zeros(env.action_space.spaces[0].n)
+        for a in range(env.action_space.spaces[0].n):
+            initial_bias[a] = initial_params_[a]
+        ## Env Wrappers | TODO: Convert to environment options
+        env = PlatformFlattenedActionWrapper(env)
         env = ScaledParameterisedActionWrapper(env)  # Parameters -> [-1,1]
+        env = ScaledStateWrapper(env)  # Observations -> [-1,1]
 
-        # Setup: Ideally minimal
-        reward_scale = 1./50.
-        pdqn_setup = {
-            # TODO: reduce redundant parts
+        ## Agent
+        pdqn_setup = {  # TODO: Find & remove redundancy
             "observation_space": env.observation_space.spaces[0],
             "action_space": env.action_space,
-            # learning_rate_actor=learning_rate_actor,  # 0.0001
-            # learning_rate_actor_param=learning_rate_actor_param,  # 0.001
-            # epsilon_steps=epsilon_steps,
-            # epsilon_final=epsilon_final,
-            # gamma=gamma,
-            # clip_grad=clip_grad,
-            # indexed=indexed,
-            # average=average,
-            # random_weighted=random_weighted,
-            # tau_actor=tau_actor,
-            # weighted=weighted,
-            # tau_actor_param=tau_actor_param,
-            # initial_memory_threshold=initial_memory_threshold,
-            # use_ornstein_noise=use_ornstein_noise,
-            # replay_memory_size=replay_memory_size,
-            # inverting_gradients=inverting_gradients,
-            "actor_kwargs": {'hidden_layers': (256,), 'output_layer_init_std': 1e-5, 'action_input_layer': 0},
-            "actor_param_kwargs": {'hidden_layers': (256,), 'output_layer_init_std': 1e-5, 'squashing_function': False},
-            # zero_index_gradients=zero_index_gradients,
+            "learning_rate_actor": 0.001,  # 0.0001
+            "learning_rate_actor_param": 0.00001,  # 0.001
+            "epsilon_steps": 1000,
+            "epsilon_final": 0.01,
+            "gamma": 0.95,
+            "clip_grad": 1,
+            "indexed": False,
+            "average": False,
+            "random_weighted": False,
+            "tau_actor": 0.1,
+            "weighted": False,
+            "tau_actor_param": 0.001,
+            "initial_memory_threshold": 128,
+            "use_ornstein_noise": True,
+            "replay_memory_size": 20000,
+            "inverting_gradients": True,
+            "actor_kwargs": {'hidden_layers': (128,),
+                             'action_input_layer': 0},
+            "actor_param_kwargs": {
+                'hidden_layers': (128,),
+                'output_layer_init_std': 1e-4,
+                'squashing_function': False},
+            "zero_index_gradients": False,
             "seed": seed
         }
         agent = PDQNAgent(**pdqn_setup)
+        agent.set_action_parameter_passthrough_weights(initial_weights, initial_bias)
 
         # Training | TODO: Simplify
-        def pad_action(act, act_param):
-            params = [np.zeros((2,)), np.zeros((1,)), np.zeros((1,))]
-            params[act] = act_param
-            return (act, params)
-        info_per_episode = []
-
-        for _ in range(train_episodes):
-            (observation, steps), info = env.reset(seed=seed)
-            observation = np.array(observation, dtype=np.float32, copy=False)
-            act, act_param, all_action_parameters = agent.act(observation)
-            action = pad_action(act, act_param)
-
-            # Episode loop
-            agent.start_episode()
-            for j in range(max_steps):
-                (next_observation, steps), reward, terminated, truncated, info = env.step(action)
-                next_observation = np.array(next_observation, dtype=np.float32, copy=False)
-
-                next_act, next_act_param, next_all_action_parameters = agent.act(next_observation)
-                next_action = pad_action(next_act, next_act_param)
-
-                r = reward * reward_scale
-                agent.step(observation, (act, all_action_parameters), r, next_observation, (next_act, next_all_action_parameters), terminated or truncated, steps)
-
-                # Reassign time-dependant variables
-                act, act_param, all_action_parameters = next_act, next_act_param, next_all_action_parameters
-                action = next_action
-                observation = next_observation
-
-                if terminated or truncated:
-                    break
-            agent.end_episode()
-            info_per_episode.append(info)
-        env.close()
+        info_per_episode = _get_training_info(train_episodes, agent, env, max_steps, seed, _pad_action)
 
         # Check
         returns = [info["episode"]["r"] for info in info_per_episode]
-        assert returns[-1] > 0.2
+        assert returns[-1] > returns[0]
+    
+
+
+def test_pdqn_goal(train_episodes=5000, max_steps=150, seeds=[1]):
+    '''Ensure P-DQN learns within Goal'''
+    for seed in seeds:
+
+        # Environment initialisation
+        env = _make_env(env_name="Goal-v0", max_steps=max_steps, seed=seed)
+
+        # Setup
+        ## Scaled actions (Unsure where the reasoning comes from for these)
+        kickto_weights = np.array([[-0.375, 0.5, 0, 0.0625, 0],
+                                   [0, 0, 0.8333333333333333333, 0, 0.111111111111111111111111]])
+        shoot_goal_left_weights = np.array([0.857346647646219686, 0])
+        shoot_goal_right_weights = np.array([-0.857346647646219686, 0])
+        ## Values
+        initial_weights = np.zeros((4, 17))
+        initial_weights[0, [10, 11, 14, 15]] = kickto_weights[0, 1:]
+        initial_weights[1, [10, 11, 14, 15]] = kickto_weights[1, 1:]
+        initial_weights[2, 16] = shoot_goal_left_weights[1]
+        initial_weights[3, 16] = shoot_goal_right_weights[1]
+        initial_bias = np.zeros((4,))
+        initial_bias[0] = kickto_weights[0, 0]
+        initial_bias[1] = kickto_weights[1, 0]
+        initial_bias[2] = shoot_goal_left_weights[0]
+        initial_bias[3] = shoot_goal_right_weights[0]
+        reward_scale = 1./50.
+        ## Env Wrappers | TODO: Convert to environment options
+        env = GoalObservationWrapper(env)
+        env = GoalFlattenedActionWrapper(env)
+        env = ScaledParameterisedActionWrapper(env)  # Parameters -> [-1,1]
+        env = ScaledStateWrapper(env)  # Observations -> [-1,1]
+
+        ## Agent
+        pdqn_setup = {  # TODO: Find & remove redundancy
+            "observation_space": env.observation_space.spaces[0],
+            "action_space": env.action_space,
+            "learning_rate_actor": 0.001,  # 0.0001
+            "learning_rate_actor_param": 0.00001,  # 0.001
+            "epsilon_steps": 1000,
+            "epsilon_final": 0.01,
+            "gamma": 0.95,
+            "clip_grad": 1,
+            "indexed": False,
+            "average": False,
+            "random_weighted": False,
+            "tau_actor": 0.1,
+            "weighted": False,
+            "tau_actor_param": 0.001,
+            "initial_memory_threshold": 128,
+            "use_ornstein_noise": True,
+            "replay_memory_size": 20000,
+            "inverting_gradients": True,
+            "actor_kwargs": {'hidden_layers': (256,), 'output_layer_init_std': 1e-5, 'action_input_layer': 0},
+            "actor_param_kwargs": {'hidden_layers': (256,), 'output_layer_init_std': 1e-5, 'squashing_function': False},
+            "zero_index_gradients": False,
+            "seed": seed
+        }
+        agent = PDQNAgent(**pdqn_setup)
+        agent.set_action_parameter_passthrough_weights(initial_weights, initial_bias)
+
+        # Training | TODO: Simplify
+        info_per_episode = _get_training_info(train_episodes, agent, env, max_steps, seed, _pad_action, reward_scale)
+
+        # Check
+        returns = [info["episode"]["r"] for info in info_per_episode]
+        assert returns[-1] > returns[0]
--- tests/test_qpamdp.py
+++ tests/test_qpamdp.py
@@ -7,64 +7,17 @@ from sarl.common.bester.environments.gym_platform.envs import PlatformEnv
 from sarl.common.bester.environments.gym_goal.envs import GoalEnv
 from sarl.common.bester.common.wrappers import QPAMDPScaledParameterisedActionWrapper
 from sarl.common.bester.common.wrappers import ScaledStateWrapper
+from sarl.common.bester.common.goal_domain import CustomFourierBasis, GoalObservationWrapper
+from sarl.common.bester.environments.gym_goal.envs.config import GOAL_WIDTH, PITCH_WIDTH, PITCH_LENGTH
 
 
-# '''
-# def _run_agent_on_env(agent, env, steps=2):
-#     (state, _), info = env.reset()
-#     for _ in range(steps):
-#         action = agent.act(state)  # this is where you would insert your policy
-#         (state, _), reward, terminated, truncated, info = env.step(action)
-
-#         if terminated or truncated:
-#             (state, _), info = env.reset()
-#     env.close()
-# '''
+def _wrap_for_qpamdp(env):
+    return QPAMDPScaledParameterisedActionWrapper(ScaledStateWrapper(env))
 
 
-# '''
-# from stable_baselines3.common.evaluation import evaluate_policy
-# from stable_baselines3.common.base_class import BaseAlgorithm
-# def _test_agent_on_env_sb3_style(env_name: str, algorithm: BaseAlgorithm, policy_name: str, save_name: str):
-#     # Based on example at https://stable-baselines3.readthedocs.io/en/master/guide/examples.html#basic-usage-training-saving-loading
-
-#     # Create environment
-#     env = gym.make(env_name)
-
-#     # Instantiate the agent
-#     model = algorithm(policy_name, env, verbose=1)
-#     # Train the agent and display a progress bar
-#     model.learn(total_timesteps=int(2e5), progress_bar=True)
-#     # Save the agent
-#     model.save(save_name)
-#     del model  # delete trained model to demonstrate loading
-
-#     # Load the trained agent
-#     # NOTE: if you have loading issue, you can pass `print_system_info=True`
-#     # to compare the system on which the model was trained vs the current one
-#     # model = DQN.load("dqn_lunar", env=env, print_system_info=True)
-#     model = algorithm.load(save_name, env=env)
-
-#     # Evaluate the agent
-#     # NOTE: If you use wrappers with your environment that modify rewards,
-#     #       this will be reflected here. To evaluate with original rewards,
-#     #       wrap environment in a "Monitor" wrapper before other wrappers.
-#     mean_reward, std_reward = evaluate_policy(
-#         model, model.get_env(), n_eval_episodes=10)
-
-#     # Enjoy trained agent
-#     vec_env = model.get_env()
-#     obs = vec_env.reset()
-#     for i in range(1000):
-#         action, _states = model.predict(obs, deterministic=True)
-#         obs, rewards, dones, info = vec_env.step(action)
-#         # vec_env.render("human")
-# '''
-
-
-def test_qpamdp_platform(train_eps=20, max_steps=201, scale=True):
+def test_qpamdp_platform(train_eps=20, max_steps=201, seeds=[1]):
     '''Ensure Q-PAMDP learns within Platform'''
-    for seed in [1, 2, 3]:
+    for seed in seeds:
         # Env init
         env = gym.make("Platform-v0")
         env = RecordEpisodeStatistics(env, deque_size=max_steps)
@@ -73,19 +26,14 @@ def test_qpamdp_platform(train_eps=20, max_steps=201, scale=True):
 
         # Setup
         initial_params = [3., 10., 400.]
-        if scale:
-            # Is this important?
-            env = ScaledStateWrapper(env)
-            variances = [0.0001, 0.0001, 0.0001]
-            for a in range(env.action_space.spaces[0].n):
-                initial_params[a] = 2. * (initial_params[a] - env.action_space.spaces[1].spaces[a].low) / (
-                            env.action_space.spaces[1].spaces[a].high - env.action_space.spaces[1].spaces[a].low) - 1.
-            env = QPAMDPScaledParameterisedActionWrapper(env)
-            alpha_param = 0.1
-        else:
-            # This case doesn't learn well?
-            variances = [0.1, 0.1, 0.01]
-            alpha_param = 1.0
+        ## Scaling
+        env = _wrap_for_qpamdp(env)
+        variances = [0.0001, 0.0001, 0.0001]
+        for a in range(env.action_space.spaces[0].n):
+            initial_params[a] = 2. * (initial_params[a] - env.action_space.spaces[1].spaces[a].low) / (
+                        env.action_space.spaces[1].spaces[a].high - env.action_space.spaces[1].spaces[a].low) - 1.
+        alpha_param = 0.1
+        ## Misc
         act_obs_index = [action_index for action_index, _ in enumerate(["self.player.position[0]",
                                                                         "self.player.velocity[0]",
                                                                         "enemy.position[0]",
@@ -122,24 +70,71 @@ def test_qpamdp_platform(train_eps=20, max_steps=201, scale=True):
         # assert returns[-1] > 0.2
 
 
-# def test_qpamdp_goal(train_eps=1, max_steps=2):
-#     '''Ensure Q-PAMDP learns within Goal'''
-#     for seed in [1, 2, 3]:
-#         # Env init
-#         env = gym.make("Goal-v0")
-#         env = RecordEpisodeStatistics(env, deque_size=max_steps)
-#         env.seed(seed)
-#         np.random.seed(seed)
+def test_qpamdp_goal(train_eps=4000, max_steps=150, seeds=[1]):
+    '''Ensure Q-PAMDP learns within Goal'''
+    for seed in seeds:
+        # Env init
+        env = gym.make("Goal-v0")
+        env = RecordEpisodeStatistics(env, deque_size=max_steps)
+        env.seed(seed)
+        np.random.seed(seed)
 
-#         # Agent init
-#         agent = QPAMDPAgent(observation_space=env.observation_space.spaces[0],
-#                             action_space=env.action_space,
-#                             seed=seed,
-#                             print_freq=100
-#                             )
 
-#         # Training
-#         info_per_episode = agent.learn(env, train_eps, max_steps)
-#         returns = [info["episode"]["r"] for info in info_per_episode]
-#         env.close()
-#         assert returns[0] < returns[-1]
+        # Setup
+        ## Values
+        variances = [0.01, 0.01, 0.01]
+        xfear = 50.0 / PITCH_LENGTH
+        yfear = 50.0 / PITCH_WIDTH
+        caution = 5.0 / PITCH_WIDTH
+        kickto_weights = np.array([[2.5, 1, 0, xfear, 0], [0, 0, 1 - caution, 0, yfear]])
+        initial_parameter_weights = [
+            kickto_weights,
+            np.array([[GOAL_WIDTH / 2 - 1, 0]]),
+            np.array([[-GOAL_WIDTH / 2 + 1, 0]])
+        ]
+
+        ## Env wrappers
+        env = GoalObservationWrapper(env)
+        env = _wrap_for_qpamdp(env)
+        ### Scaling
+        variances[0] = 0.0001
+        variances[1] = 0.0001
+        variances[2] = 0.0001
+        alpha_param = 0.06
+        initial_parameter_weights[0] = np.array([[-0.375, 0.5, 0, 0.0625, 0],
+                                [0, 0, 0.8333333333333333333, 0, 0.111111111111111111111111]])
+        initial_parameter_weights[1] = np.array([0.857346647646219686, 0])
+        initial_parameter_weights[2] = np.array([-0.857346647646219686, 0])
+
+        ## Components
+        action_obs_index = np.arange(14)
+        param_obs_index = np.array([
+            np.array([10, 11, 14, 15]),  # ball_features
+            np.array([16, 0, 0, 0]),  # keeper_features  # Padded w/ zeroes
+            np.array([16, 0, 0, 0]),  # keeper_features  # to stop error
+        ])
+        basis = CustomFourierBasis(14, env.observation_space.spaces[0].low[:14], env.observation_space.spaces[0].high[:14])
+        discrete_agent = SarsaLambdaAgent(env.observation_space.spaces[0], env.action_space.spaces[0], basis=basis, seed=seed, alpha=0.01,
+                                        lmbda=0.1, gamma=0.9, temperature=1.0, cooling=1.0, scale_alpha=False,
+                                        use_softmax=True,
+                                        observation_index=action_obs_index, gamma_step_adjust=False)
+
+
+        # Agent init
+        agent = QPAMDPAgent(env.observation_space.spaces[0], env.action_space, alpha=alpha_param, initial_action_learning_episodes=4000,
+                            seed=seed, action_obs_index=action_obs_index, parameter_obs_index=param_obs_index,
+                            variances=variances, discrete_agent=discrete_agent, action_relearn_episodes=2000,
+                            parameter_updates=1000, parameter_rollouts=50, norm_grad=True, print_freq=100,
+                            phi0_func=lambda state: np.array([1, state[1], state[1]**2]),
+                            phi0_size=3)
+        # agent = QPAMDPAgent(observation_space=env.observation_space.spaces[0],
+        #                     action_space=env.action_space,
+        #                     seed=seed,
+        #                     print_freq=100
+        #                     )
+
+        # Training
+        info_per_episode = agent.learn(env, train_eps, max_steps)
+        returns = [info["episode"]["r"] for info in info_per_episode]
+        env.close()
+        assert returns[0] < returns[-1]
