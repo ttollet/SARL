e9b2926774b1efa4ec23a7dae948dc8f578f9967
Theodore Tollet <ttollet@icloud.com>
2024-04-15 10:42:24 +0100

    Hybrid-policy class (in progress!)

--- sarl/wrappers/converter.py
+++ sarl/wrappers/converter.py
@@ -3,10 +3,64 @@ from gymnasium import Env, Wrapper, spaces
 
 STEP_KEYS = ["obs", "reward", "terminated", "truncated", "info"]
 
-class PamdpToMdp(Wrapper):
-    def expectingDiscreteAction(self):
-        return self.previous_step_output["obs"][0] == 0
 
+class HybridPolicy:
+    # For combining two separate policies for use with the converter
+    def __init__(self, discretePolicy, continuousPolicy) -> None:
+        self.discretePolicy = discretePolicy
+        self.continuousPolicy = continuousPolicy
+
+    def act(self, obs):
+        if obs[0]==0:
+            return self.discretePolicy(obs[1])
+        else:
+            assert obs[0]==1  # Comment out for faster computation
+            return self.continuousPolicy(obs[1])
+        
+
+class PamdpToMdpView(Env):
+    def __init__(self, parent: Env, action_space_is_discrete: bool, internal_policy=None) -> None:
+        super().__init__()
+        if action_space_is_discrete:
+            self.action_space = parent.discrete_action_space
+        else:
+            self.action_space = parent.action_parameter_space
+        self.observation_space = parent.observation_space[1]
+        self.reward_range = parent.reward_range  # TODO: Amend in future
+        self.spec = parent.spec
+        self.metadata = parent.metadata
+        self.np_random = parent.np_random
+
+        if internal_policy == None:
+            if action_space_is_discrete:
+                self.internal_policy = self.action_parameter_space.sample
+            else:
+                self.internal_policy = self.action_parameter_space.sample
+        else:
+            self.internal_policy = internal_policy
+        self.action_space_is_discrete = action_space_is_discrete
+        self.parent = parent
+        if action_space_is_discrete:
+            assert parent.expectingDiscreteAction()
+        else:
+            assert not parent.expectingDiscreteAction()
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = self.parent.step(action)
+        return self.parent.step(self.internal_policy(obs))
+
+    def reset(self):
+        return self.parent.reset()
+    
+    def render(self):
+        return self.parent.render()
+    
+    def close(self):
+        return self.parent.close()
+
+
+
+class PamdpToMdp(Wrapper):
     def __init__(self, env: Env):
         super().__init__(env)
 
@@ -16,8 +70,20 @@ class PamdpToMdp(Wrapper):
             original_observation_space
         ))
 
+        self.discrete_action_space = self.action_space[0]
+        self.action_parameter_space = self.action_space[1]
+        # self.action_space = 
+
         self.previous_step_output = {key: None for key in STEP_KEYS}
         self.discrete_action_choice = None
+
+
+    def getComponentMdp(self, action_space_is_discrete: bool, internal_policy=None) -> Env:
+        return PamdpToMdpView(self, action_space_is_discrete, internal_policy)
+
+
+    def expectingDiscreteAction(self):
+        return self.previous_step_output["obs"][0] == 0
 
     
     def reset(self):
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -2,34 +2,53 @@ import numpy as np
 import gymnasium as gym
 from gymnasium.wrappers import RecordEpisodeStatistics  # Note stats
 
-from sarl.wrappers.converter import PamdpToMdp
+from sarl.wrappers.converter import PamdpToMdp, HybridPolicy
 from sarl.common.bester.environments.gym_platform.envs import PlatformEnv
 from sarl.common.bester.environments.gym_goal.envs import GoalEnv
 
 
-def _make_env(env_name: str, max_steps: int, seed: int):
+def _make_env(env_name: str, seed: int):
     env = gym.make(env_name)
     env.seed(seed)  # Remove stochasticity
     np.random.seed(seed)
     return env
 
 
-def test_converter_sample(max_steps:int=5, seed:int=42) -> None:
-    '''Converter can be sampled from'''
+def test_converter_agent_sample(max_steps:int=5, seed:int=42) -> None:
+    '''Hybrid policy class can correctly sample converter action space'''
     for env_name in ["Platform-v0", "Goal-v0"]:
-        pamdp = _make_env(env_name=env_name, max_steps=max_steps, seed=seed)
+        pamdp = _make_env(env_name=env_name, seed=seed)
         mdp = PamdpToMdp(pamdp)
-
-        # For clarity
-        discrete_action_space = mdp.action_space[0]
-        action_parameter_space = mdp.action_space[1]
+        agent = HybridPolicy(
+            discretePolicy = lambda x: mdp.discrete_action_space.sample(),
+            continuousPolicy = lambda x: mdp.action_parameter_space.sample()
+        )
 
         obs, info = mdp.reset()
         for i in range(max_steps*2):
             if mdp.expectingDiscreteAction():
-                obs, reward, terminated, truncated, info = mdp.step(discrete_action_space.sample())
+                obs, reward, terminated, truncated, info = mdp.step(agent.act(obs))
             else:
-                obs, reward, terminated, truncated, info = mdp.step(action_parameter_space.sample())
+                obs, reward, terminated, truncated, info = mdp.step(agent.act(obs))
+    return None
+
+
+def test_converter_discrete_learning(max_steps:int=500, seed:int=42) -> None:
+    '''Hybrid policy class can correctly sample converter action space'''
+    for env_name in ["Platform-v0", "Goal-v0"]:
+        pamdp = _make_env(env_name=env_name, seed=seed)
+        mdp = PamdpToMdp(pamdp)
+        agent = HybridPolicy(
+            discretePolicy = lambda x: mdp.discrete_action_space.sample(),
+            continuousPolicy = lambda x: mdp.action_parameter_space.sample()
+        )
+
+        obs, info = mdp.reset()
+        for i in range(max_steps*2):
+            if mdp.expectingDiscreteAction():
+                obs, reward, terminated, truncated, info = mdp.step(agent.act(obs))
+            else:
+                obs, reward, terminated, truncated, info = mdp.step(agent.act(obs))
     return None
 
 
