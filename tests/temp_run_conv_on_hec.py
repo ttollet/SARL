63e8a1870c4cf33a48e73a6a51043512bc360c8b
Tollet  Theodore <tollet@wayland-2022.hec.lancs.ac.uk>
2024-12-01 21:28:08 +0000

    HEC results

--- tests/temp_run_conv_on_hec.py
+++ tests/temp_run_conv_on_hec.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 from tests.test_converter import test_converter_both
 
 # learning_steps=250*2*3 is 53s on M1 MBP
 CYCLES = 10
 TARGET_MINUTES = 60
-TIMESTEPS = 250*2*3 * TARGET_MINUTES * CYCLES
-test_converter_both(discreteAlg="PPO", continuousAlg="PPO", learning_steps=TIMESTEPS, log_results=True, cycles=CYCLES)
-test_converter_both(discreteAlg="A2C", continuousAlg="PPO", learning_steps=TIMESTEPS, log_results=True, cycles=CYCLES)
+
+timesteps  = 250*2*3 * TARGET_MINUTES * CYCLES
+test_converter_both(discreteAlg="PPO", continuousAlg="PPO", learning_steps=timesteps, log_results=True, cycles=CYCLES)
+test_converter_both(discreteAlg="A2C", continuousAlg="PPO", learning_steps=timesteps, log_results=True, cycles=CYCLES)
