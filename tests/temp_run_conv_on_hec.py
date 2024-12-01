9edf34275f7292aecdecb81fca908211c38babf6
Tollet  Theodore <tollet@wayland-2022.hec.lancs.ac.uk>
2024-12-01 22:46:09 +0000

    HEC job script now can specify alg combinations for convertor & hybrid policy

--- tests/temp_run_conv_on_hec.py
+++ tests/temp_run_conv_on_hec.py
@@ -4,16 +4,20 @@ from tests.test_converter import test_converter_both
 
 # learning_steps=250*2*3 is 53s on M1 MBP
 CYCLES = 10
-TARGET_MINUTES = 60
+TARGET_MINUTES = 60 * 10
 
 # Allow seed to be passed via command line
-if len(sys.argv) == 1:
-	seed = 42
-else:
-	assert sys.argv == 2
-	seed = sys.argv[1]
+seed = 42
+args = sys.argv
+if len(args) > 1:
+        print(f"Sys.argv = {sys.argv}")
+        seed:int = int(args[1])
+        discreteAlg = args[2]
+        continuousAlg = args[3]
+print(f"Seed = {seed}")
 
 timesteps  = 250*2*3 * TARGET_MINUTES * CYCLES
 
-test_converter_both(discreteAlg="PPO", continuousAlg="PPO", learning_steps=timesteps, log_results=True, cycles=CYCLES, seed=)
-test_converter_both(discreteAlg="A2C", continuousAlg="PPO", learning_steps=timesteps, log_results=True, cycles=CYCLES)
+test_converter_both(discreteAlg=discreteAlg, continuousAlg=continuousAlg, learning_steps=timesteps, log_results=True, cycles=CYCLES, seed=seed)
+#test_converter_both(discreteAlg="PPO", continuousAlg="PPO", learning_steps=timesteps, log_results=True, cycles=CYCLES, seed=seed)
+#test_converter_both(discreteAlg="A2C", continuousAlg="PPO", learning_steps=timesteps, log_results=True, cycles=CYCLES, seed=seed)
