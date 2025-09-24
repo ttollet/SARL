3d5a01c7ffea6aa7ff72e86789e94538fe9f7ef7
Theodore Tollet <ttollet@icloud.com>
2025-09-24 19:53:03 +0100

    Update pdqn_use.py

--- sarl/snippets/pdqn_use.py
+++ sarl/snippets/pdqn_use.py
@@ -61,7 +61,7 @@ def _get_training_info(train_episodes, agent, env, max_steps, seed, pad_action, 
     if eval_episodes is None:
         eval_episodes = 15
     EVAL_FREQ = 12288+1 # 100 # 500
-    EVAL_FREQ = 1000+1 # 100 # 500
+    # EVAL_FREQ = 1000+1 # 100 # 500
     if output_dir:
         pass
         # writer = SummaryWriter(log_dir=output_dir)
@@ -258,4 +258,4 @@ def pdqn_goal(train_episodes=5000, max_steps=150, seeds=[1], output_dir=True, le
         eval_env.close()
         return returns
 
-# pdqn_goal(train_episodes=1000000, max_steps=500, seeds=[1], output_dir=True, learning_steps=None, cycles=None, eval_episodes=250)
+pdqn_goal(train_episodes=1000000, max_steps=500, seeds=[1], output_dir=True, learning_steps=None, cycles=None, eval_episodes=250)
